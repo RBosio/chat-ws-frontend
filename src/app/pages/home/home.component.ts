a9454e8b2c99e8f56f5ca1b490b0a496c85851ce
Fiido <roccobosio@gmail.com>
2024-03-04 18:02:39 -0300

    change home design and font

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgIf } from "@angular/common"
+import { Component, OnInit } from "@angular/core"
+import { RouterModule } from "@angular/router"
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  imports: [RouterModule],
-  templateUrl: './home.component.html',
+  imports: [RouterModule, NgIf],
+  templateUrl: "./home.component.html",
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  id!: number
 
+  ngOnInit(): void {
+    this.id = Number(localStorage.getItem("sub"))
+  }
 }
