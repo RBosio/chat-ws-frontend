490c617d59a6287a9321b96752b3c88bfe39bc3e
Fiido <roccobosio@gmail.com>
2024-03-04 16:23:56 -0300

    init ngx-socket-io

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core"
 import { provideRouter } from "@angular/router"
+import { SocketIoModule } from "ngx-socket-io"
 
 import { routes } from "./app.routes"
 import { HttpClientModule } from "@angular/common/http"
@@ -7,6 +8,9 @@ import { HttpClientModule } from "@angular/common/http"
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(
+      HttpClientModule,
+      SocketIoModule.forRoot({ url: "http://localhost:3000", options: {} })
+    ),
   ],
 }
