1f0f7c8906b008b7b1d8966bf1f0b0b27c064f39
Fiido <roccobosio@gmail.com>
2024-03-04 16:24:31 -0300

    socket ok

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from "@angular/router"
 import { NgClass, NgFor } from "@angular/common"
 import { ButtonComponent } from "../button/button.component"
 import { FormsModule } from "@angular/forms"
+import { Socket } from "ngx-socket-io"
 
 @Component({
   selector: "app-chat",
@@ -17,14 +18,28 @@ export class ChatComponent implements OnInit {
   id!: number
   message!: string
   groupId!: number
+  groupName!: string
 
   ngOnInit(): void {
     this.id = Number(localStorage.getItem("sub"))
+    this.socket.fromEvent("message").subscribe((res: any) => {
+      const m = {
+        message: res.message,
+        userSend: {
+          id: res.userId,
+        },
+        created_at: res.created_at,
+      }
+      this.messages.push(m)
+    })
 
     this.route.params.subscribe((params) => {
-      this.groupService.getGroup(params["name"]).subscribe((res) => {
+      this.groupName = params["name"]
+      this.groupService.getGroup(this.groupName).subscribe((res) => {
         this.messages = res.messages
         this.groupId = res.id
+
+        this.socket.emit("join", this.groupName)
       })
     })
   }
@@ -34,6 +49,12 @@ export class ChatComponent implements OnInit {
       this.groupService
         .sendMessage(this.message, this.groupId, this.id)
         .subscribe((res) => {
+          this.socket.emit("message", {
+            userId: this.id,
+            groupId: this.groupName,
+            message: this.message,
+          })
+
           this.message = ""
         })
     }
@@ -41,6 +62,7 @@ export class ChatComponent implements OnInit {
 
   constructor(
     private groupService: GroupService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private socket: Socket
   ) {}
 }
